eb0fdb6c56907b1e388e13a98a1da639f1eb2c67
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-05-11 21:20:54 -0300

    Adicionar arquivos de projeto.

---
+++ Picker/Picker/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Picker
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set {SetProperty(ref isBusy , value); }
+        }
+
+        public Command CarregarCommand { get; }
+
+        public ObservableCollection<string> Itens { get; }
+
+        public MainViewModel()
+        {
+            Itens           = new ObservableCollection<string>();
+            CarregarCommand = new Command(async () => await ExecuteCarregarCommand(), () => !isBusy);
+        }
+
+        async Task ExecuteCarregarCommand()
+        {
+
+            if (!IsBusy)
+            {
+                try
+                {
+                    IsBusy = true;
+                    var minhaLista = await Servico.CarregarString();
+                    foreach (var item in minhaLista)
+                    {
+                        Itens.Add(item);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    await DisplayAlert("Erro", $"Erro:{ex.Message}", "Ok");
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+            return;
+        }
+    }
+
+
+
+    public class Servico
+    {
+        public static async Task<List<string>> CarregarString()
+        {
+            var lista = new List<string> { "oi", "olá", "tudo bem?" };
+            
+            //Simula uma requisição Http
+            await Task.Delay(6000);
+            return lista;
+        }
+    }
+}

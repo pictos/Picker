0e3e285aae395b6fbdc733f3ccc3e87336698e3f
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-05-13 00:34:44 -0300

    ItemSelected

--- Picker/Picker/MainViewModel.cs
+++ Picker/Picker/MainViewModel.cs
@@ -14,17 +14,55 @@ namespace Picker
         public bool IsBusy
         {
             get { return isBusy; }
-            set {SetProperty(ref isBusy , value); }
+            set { SetProperty(ref isBusy, value); }
         }
+
+        private string item;
+
+        public string Item
+        {
+            get { return item; }
+            set { SetProperty(ref item, value); }
+        }
+
 
         public Command CarregarCommand { get; }
 
+        public Command SelecionarCommand { get; }
+
         public ObservableCollection<string> Itens { get; }
+
+
 
         public MainViewModel()
         {
-            Itens           = new ObservableCollection<string>();
-            CarregarCommand = new Command(async () => await ExecuteCarregarCommand(), () => !isBusy);
+            Itens             = new ObservableCollection<string>();
+            CarregarCommand   = new Command(async () => await ExecuteCarregarCommand(), () => !isBusy);
+            SelecionarCommand = new Command(async () => await ExecuteSelecionarCommand(), () => !IsBusy);
+
+        }
+
+        async Task ExecuteSelecionarCommand()
+        {
+
+            if (!IsBusy)
+            {
+                try
+                {
+                    IsBusy = true;
+                    await DisplayAlert("Selecionado", $"Você clicou em: {Item.ToString()}");
+                }
+                catch (Exception ex)
+                {
+
+                    await DisplayAlert("Erro", $"Erro:{ex.Message}", "Ok");
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+            return;
         }
 
         async Task ExecuteCarregarCommand()
@@ -63,7 +101,7 @@ namespace Picker
         public static async Task<List<string>> CarregarString()
         {
             var lista = new List<string> { "oi", "olá", "tudo bem?" };
-            
+
             //Simula uma requisição Http
             await Task.Delay(6000);
             return lista;
